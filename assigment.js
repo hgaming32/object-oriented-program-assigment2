37e51c4cb16daed2839d01b716fb430e00034ada
hgaming32 <alkimaabdi666@gmail.com>
2024-10-29 08:56:30 -0700

    Add files via upload

---
+++ assigment.js
@@ -0,0 +1,37 @@
+/*
+getSecondBookAvalibe() {
+    this.#books.forEach(book => console.log(book.title));
+}
+
+task 4 
+*/
+
+/*
+/*
+   getSecondBookAvalibe() {
+      return this.#books.filter(book => book.isAvailable);
+   }
+}
+task 5
+*/
+
+
+/*
+
+getSecondBookAvalibe() {
+   const book = this.#books.filter(book => book.title === "1984");
+   
+      book.borrowers.push({ name: "Tom Hanks", borrowedDate: "2024-10-01", returnDate: "2024-10-31"  });
+
+}
+      // task 6
+ */
+
+// didnt know how to do 7 8 or 9
+
+/*
+getSecondBookAvalibe() {
+    return this.#staff.map(member => `${member.name} - ${member.role}`);
+ }
+ */
+
